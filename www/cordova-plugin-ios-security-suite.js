b9a3b01b6383f13cf0eb629f5e0aeeced8732af2
dishant-kaushik <dishant.kaushik@ibm.com>
2020-05-11 13:11:58 -0400

    export js part as a module

--- www/cordova-plugin-ios-security-suite.js
+++ www/cordova-plugin-ios-security-suite.js
@@ -1,5 +1,9 @@
 var exec = require("cordova/exec");
 
-exports.coolMethod = function (arg0, success, error) {
-  exec(success, error, "IOSSecuritySuite", "isRooted", [arg0]);
+const IOSSecuritySuite = {
+  isRooted: function (arg0, success, error) {
+    exec(success, error, "IOSSecuritySuite", "isRooted", [arg0]);
+  },
 };
+
+export default IOSSecuritySuite;
