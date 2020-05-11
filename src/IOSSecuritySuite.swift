d516a601e93f56287583ff6e014aa1b7e6675d54
dishant-kaushik <dishant.kaushik@ibm.com>
2020-05-11 12:42:21 -0400

    added IOSSecuritySuite while creating a swift plugin

---
+++ src/IOSSecuritySuite.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+@objc(ModusEchoSwift) class ModusEchoSwift: CDVPlugin{
+    func isRooted(command: CDVInvokedUrlCommand) {
+        var pluginResult = CDVPluginResult(
+            status: CDVCommandStatus_ERROR
+        )
+
+        let msg = command.argumends[0] as? String ?? ""
+
+        pluginResult = CDVPluginResult(
+            status: CDVCommandStatus_OK,
+            messageAsBool: JailbreakChecker.amIJailbroken()
+        )
+        self.commandDelegate!.sendPluginResult(
+            pluginResult,
+            callbackId: command.callbackId
+        )
+    }
+}
